fc829868f6dfd0895dd45b15f002967a07417694
root <root@ubuntu-24.localhost>
2025-09-12 11:11:15 +0000

    Logs added by LogWrite

--- src/main/java/com/jobportal/dao/SearchDAO.java
+++ src/main/java/com/jobportal/dao/SearchDAO.java
@@ -11,9 +11,11 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.TextCriteria;
 import org.springframework.data.mongodb.core.query.TextQuery;
 import org.springframework.stereotype.Repository;
+import lombok.extern.slf4j.Slf4j;
 
 
 @Repository
+@Slf4j
 public class SearchDAO {
 	
 	@Autowired
@@ -21,6 +23,7 @@ public class SearchDAO {
 
 	public <T> List<T> searchByTerm(Class className, String term, Pageable p)
 	{
+		log.debug("ENTERING: searchByTerm() method");
 		TextCriteria criteria = TextCriteria.forDefaultLanguage()
 				.matchingAny(term.split("\\s+"));  // handles multiple words
 
@@ -28,8 +31,10 @@ public class SearchDAO {
 				.with(Sort.by(Sort.Direction.DESC, "updatedOn"))
 				.with(p);
 
+		log.info("Database operation: FIND - find");
 		List<T> result = template.find(query, className);
 
+		log.info("EXITING: searchByTerm() method - Successfully completed");
 		return result;
 	}
 }
